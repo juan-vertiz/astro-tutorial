8a44885f8ec4ae2dd9a47c62700a35862a544f80
Juan Vertiz <juanpablovertiz@hotmail.com>
2025-10-10 21:43:35 -0600

    add(cicd): add deployment workflow for deploying site to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://juan-vertiz.github.io',
+  base: '/astro-tutorial',
+});
