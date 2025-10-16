6da5d75f2160785afe2baebb426fde38267123da
Juan Vertiz <juanpablovertiz@hotmail.com>
2025-10-15 23:28:20 -0600

    update: migrate post pages to collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
   return rss({
     title: 'Juan Vertiz | Blog',
     description: 'My journey learning backend development',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/astro-tutorial/blog/${post.id}`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
